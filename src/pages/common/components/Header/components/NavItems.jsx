cf0617aed95aab41418940e14db554dc47b3e0e3
Maksims Turs <maksims07turs@gmail.comm>
2023-03-11 13:08:48 +0100

    Commit

--- src/pages/common/components/Header/components/NavItems.jsx
+++ src/pages/common/components/Header/components/NavItems.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function NavItems({props, isActiveLink, activateLink}) {
     return (
-        <li className="header__nav-item" onClick={activateLink}>
+        <li className="header__nav-item" onClick={PactivateLink}>
             <a href="#" className="header__item-link">
                 <div className="header__item-content">
                     <img src={props} alt="Home icon" className="header__item-icon" />
